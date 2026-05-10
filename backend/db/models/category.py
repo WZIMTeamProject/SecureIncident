8cf9f307ca425bbbe61a81a3f6fa73398417dbf9
Natan Misztal <natan.misztal.c@gmail.com>
2026-05-10 17:42:41 +0200

    Change primary keys and foreign keys to UUID
    Updated category model to use UUIDs for primary keys and foreign keys.

--- backend/db/models/category.py
+++ backend/db/models/category.py
@@ -1,32 +1,34 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, Table
+from sqlalchemy import Column, String, Text, ForeignKey, DateTime, Table
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from backend.db.base import Base
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 
 # Association table for many-to-many relationship between helper and category
 helper_category = Table(
     "helper_category",
     Base.metadata,
-    Column("project_id", Integer, ForeignKey("project.id"), primary_key=True),
-    Column("helper_id", Integer, ForeignKey("user.id"), primary_key=True),
-    Column("category_id", Integer, ForeignKey("category.id"), primary_key=True),
+    Column("project_id", UUID(as_uuid=True), ForeignKey("project.id"), primary_key=True),
+    Column("helper_id", UUID(as_uuid=True), ForeignKey("user.id"), primary_key=True),
+    Column("category_id", UUID(as_uuid=True), ForeignKey("category.id"), primary_key=True),
 )
 
 # Association table for many-to-many relationship between incident and category
 incident_category = Table(
     "incident_category",
     Base.metadata,
-    Column("incident_id", Integer, ForeignKey("incident.id"), primary_key=True),
-    Column("category_id", Integer, ForeignKey("category.id"), primary_key=True),
+    Column("incident_id", UUID(as_uuid=True), ForeignKey("incident.id"), primary_key=True),
+    Column("category_id", UUID(as_uuid=True), ForeignKey("category.id"), primary_key=True),
 )
 
 
 class Category(Base):
     __tablename__ = "category"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    project_id = Column(Integer, ForeignKey("project.id"), nullable=False)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    project_id = Column(UUID(as_uuid=True), ForeignKey("project.id"), nullable=False)
     name = Column(String(50), nullable=False)
     description = Column(Text, nullable=False)
     created_at = Column(DateTime, server_default=func.now())
