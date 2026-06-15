2349115c09208f03c6f82474df1216270a820fb4
Cryosmium <45072703+Cryosmium@users.noreply.github.com>
2026-06-15 18:11:30 +0200

    Post merge and rebase fixes to SIRegisterPage.tsx

--- Frontend/src/login/SIRegisterPage.tsx
+++ Frontend/src/login/SIRegisterPage.tsx
@@ -1,16 +1,25 @@
-import {Form, Link} from "react-router";
-import {FORM_EMAIL, FORM_PASSWORD, FORM_USERNAME} from "./forms.ts";
+import {Link, useFetcher} from "react-router";
+import {FORM_EMAIL, FORM_FIRST_NAME, FORM_LAST_NAME, FORM_PASSWORD, FORM_USERNAME} from "./forms.ts";
 import {Background} from "../components/Background.tsx";
 
 export function SIRegisterPage() {
-    // TODO: Account creation page functionality, should also probably redirect if logged in?
+    const fetcher = useFetcher();
+    const busy = fetcher.state !== "idle";
 
     return <Background>
         <div className={`w-lg p-2 bg-purple-400 text-black`}>
-            <Form>
+            <fetcher.Form method="POST">
                 <div className={`m-2 flex flex-col`}>
                     <label htmlFor={FORM_USERNAME}>Podaj swoją nazwę użytkownika:</label>
                     <input type="text" name={FORM_USERNAME} className={`bg-white`}/>
+                </div>
+                <div className={`m-2 flex flex-col`}>
+                    <label htmlFor={FORM_FIRST_NAME}>Podaj swoje imię:</label>
+                    <input type="text" name={FORM_FIRST_NAME} className={`bg-white`}/>
+                </div>
+                <div className={`m-2 flex flex-col`}>
+                    <label htmlFor={FORM_LAST_NAME}>Podaj swoje nazwisko:</label>
+                    <input type="text" name={FORM_LAST_NAME} className={`bg-white`}/>
                 </div>
                 <div className={`m-2 flex flex-col`}>
                     <label htmlFor={FORM_EMAIL}>Podaj adres e-mail:</label>
@@ -25,10 +34,12 @@ export function SIRegisterPage() {
                         <input
                             className={`p-2 cursor-pointer bg-purple-500 text-white underline`}
                             type="submit"
-                            value="Zarejestruj się"/>
+                            value={busy ? "Rejestrowanie..." : "Zarejestruj się"}
+                            disabled={busy}
+                            />
                     </div>
                 </div>
-            </Form>
+            </fetcher.Form>
         </div>
         <div>
             <Link to="/login">Mam już konto</Link>
