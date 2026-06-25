1698fc92b8e6f5c6581b2bbadebef4e938292085
Cryosmium <45072703+Cryosmium@users.noreply.github.com>
2026-06-25 18:48:03 +0200

    Added role creation popup (NYI)

--- Frontend/src/dashboard/SIProject.tsx
+++ Frontend/src/dashboard/SIProject.tsx
@@ -4,11 +4,11 @@ import {Link, useFetcher, useParams} from "react-router";
 import Api from "../data/Api.ts";
 import {
     FORM_ACTION,
-    FORM_ACTION_NEW_INCIDENT,
+    FORM_ACTION_NEW_INCIDENT, FORM_ACTION_NEW_ROLE,
     FORM_INCIDENT_ASSIGNEES,
     FORM_INCIDENT_DESCRIPTION,
     FORM_INCIDENT_NAME,
-    FORM_INCIDENT_PRIORITY, FORM_PROJECT_ID,
+    FORM_INCIDENT_PRIORITY, FORM_PROJECT_ID, FORM_ROLE_NAME, FORM_ROLE_PERMISSION,
 } from "./forms.ts";
 import {Popup} from "../components/Popup.tsx";
 import * as React from "react";
@@ -116,7 +116,7 @@ function ProjectView({project}: { project?: Project }) {
                 </p>
             </div>
 
-            <div className="text-lg font-bold px-6 py-2
+            <div className="text-lg font-bold px-6 py-2 transition-colors duration-300
                 bg-(--color-si-card-border) text-(--color-si-card-bg)
                 rounded-t-lg w-fit shadow-lg">
                 Incydenty
@@ -126,7 +126,7 @@ function ProjectView({project}: { project?: Project }) {
 
             <div className="flex flex-row justify-center gap-3">
                 <div className={"flex-1"}>
-                    <div className="text-lg font-bold px-6 py-2
+                    <div className="text-lg font-bold px-6 py-2 transition-colors duration-300
                         bg-(--color-si-card-border) text-(--color-si-card-bg)
                         rounded-t-lg w-fit shadow-lg">
                         Członkowie
@@ -135,7 +135,7 @@ function ProjectView({project}: { project?: Project }) {
                 </div>
 
                 <div className={"flex-1"}>
-                    <div className="text-lg font-bold px-6 py-2
+                    <div className="text-lg font-bold px-6 py-2 transition-colors duration-300
                         bg-(--color-si-card-border) text-(--color-si-card-bg)
                         rounded-t-lg w-fit shadow-lg">
                         Role
@@ -166,11 +166,7 @@ function IncidentList({show, project, incidents}: {show: boolean, project?: Proj
                     rounded-2xl rounded-tl-none shadow-lg px-8 py-8 transition-colors duration-300 overflow-y-scroll text-(--color-si-input-text)">
                 {
                     incidents?.map((incident) => {
-                        return <p>
-                            <Link to={`/dashboard/incident/${incident.id}`} className={"hover:underline"}>
-                                {incident.title} - {incident.id}
-                            </Link>
-                        </p>;
+                        return <IncidentEntry incident={incident}/>;
                     }) ?? <h1>Ładowanie...</h1>
                 }
             </div>
@@ -188,6 +184,14 @@ function IncidentList({show, project, incidents}: {show: boolean, project?: Proj
             </div>
         </div>
     );
+}
+
+function IncidentEntry({incident}: {incident: Incident}) {
+    return <div>
+        <Link to={`/dashboard/incident/${incident.id}`} className={"hover:underline"}>
+            {incident.title} - {incident.id}
+        </Link>
+    </div>
 }
 
 function MemberList({show, project, members} : {show: boolean, project?: Project, members?: ProjectMemberResponse[]}) {
@@ -222,10 +226,19 @@ function MemberList({show, project, members} : {show: boolean, project?: Project
 }
 
 function RoleList({show, project, roles} : {show: boolean, project?: Project, roles?: RoleResponse[]}) {
+    const [showPopup, setShowPopup] = useState<boolean>(false);
+    const hidePopup = () => setShowPopup(false);
+
     const disableButtons: boolean = project === undefined;
 
     return (
         <div hidden={!show} className={"flex-1"}>
+            {
+                project
+                    ? <NewRolePopup show={showPopup} onHide={hidePopup} project={project}/>
+                    : undefined
+            }
+
             <div className="w-full h-96
                     border-5 border-(--color-si-card-border)
                     rounded-2xl rounded-tl-none shadow-lg px-8 py-8 transition-colors duration-300 overflow-y-scroll text-(--color-si-input-text)">
@@ -240,6 +253,7 @@ function RoleList({show, project, roles} : {show: boolean, project?: Project, ro
 
             <div className="w-full flex gap-3 p-3 justify-end">
                 <button
+                    onClick={() => setShowPopup(true)}
                     disabled={disableButtons}
                     className="px-6 py-2
                         bg-(--color-si-btn)
@@ -394,6 +408,188 @@ function NewIncidentPopup({show, onHide, project}: { show: boolean, onHide: () =
                     name={FORM_ACTION}
                     type="hidden"
                     value={FORM_ACTION_NEW_INCIDENT}/>
+            </fetcher.Form>
+        </Popup>
+    );
+}
+
+function NewRolePopup({show, onHide, project} : {show: boolean, onHide: () => void, project: Project}) {
+    const fetcher = useFetcher();
+    const busy = fetcher.state != "idle";
+
+    const [pendingHide, setPendingHide] = useState<boolean>(false);
+
+    const roleNameRef = useRef<HTMLInputElement>(null);
+    const rolePermWriteTicketRef = useRef<HTMLInputElement>(null);
+    const rolePermHelpRef = useRef<HTMLInputElement>(null);
+    const rolePermAssignHelpRef = useRef<HTMLInputElement>(null);
+    const rolePermChangeStatusRef = useRef<HTMLInputElement>(null);
+    const rolePermMakeRoleRef = useRef<HTMLInputElement>(null);
+    const rolePermChangeRoleRef = useRef<HTMLInputElement>(null);
+    const rolePermAssignToProjectRef = useRef<HTMLInputElement>(null);
+
+    if (fetcher.state == "idle" && pendingHide) {
+        setPendingHide(false);
+        onHide();
+    }
+
+    return (
+        <Popup show={show} className={"w-full max-w-xl"}>
+            <fetcher.Form method="POST" onSubmit={() => setPendingHide(true)}>
+                <h1 className="text-3xl font-bold text-(--color-si-label)">
+                    Tworzenie nowej roli w projekcie {project.name}.
+                </h1>
+
+                <div className="flex flex-col gap-1.5 my-3">
+                    <label htmlFor={FORM_ROLE_NAME} className="text-sm font-medium text-(--color-si-label)">
+                        Podaj nazwę roli:
+                    </label>
+                    <div className="flex items-center gap-3
+                                border border-(--color-si-input-border)
+                                rounded-lg px-3 py-2.5
+                                bg-(--color-si-input-bg) transition-colors">
+                        <input
+                            ref={roleNameRef}
+                            id={FORM_ROLE_NAME}
+                            type="text"
+                            required={true}
+                            name={FORM_ROLE_NAME}
+                            placeholder="Nazwa"
+                            className="flex-1 bg-transparent outline-none text-sm text-(--color-si-input-text)"
+                        />
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-1.5 my-3">
+                    <label htmlFor={FORM_ROLE_PERMISSION} className="text-sm font-medium text-(--color-si-label)">
+                        Pozwolenia:
+                    </label>
+
+                    <div className="flex flex-wrap gap-y-2
+                                text-sm font-medium text-(--color-si-label)
+                                border border-(--color-si-input-border)
+                                rounded-lg px-3 py-2.5
+                                bg-(--color-si-input-bg) transition-colors">
+
+                        <div className="flex gap-1 items-center flex-1/2">
+                            <input
+                                type="checkbox"
+                                ref={rolePermWriteTicketRef}
+                                name={FORM_ROLE_PERMISSION}
+                                value="write_ticket"
+                                className="w-4 h-4 bg-(--color-si-input-bg) accent-(--color-si-btn) cursor-pointer"
+                            />
+                            Tworzenie ticketów
+                        </div>
+
+                        <div className="flex gap-1 items-center flex-1/2">
+                            <input
+                                type="checkbox"
+                                ref={rolePermHelpRef}
+                                name={FORM_ROLE_PERMISSION}
+                                value="help"
+                                className="w-4 h-4 bg-(--color-si-input-bg) accent-(--color-si-btn) cursor-pointer"
+                            />
+                            Pomaganie
+                        </div>
+
+                        <div className="flex gap-1 items-center flex-1/2">
+                            <input
+                                type="checkbox"
+                                ref={rolePermAssignHelpRef}
+                                name={FORM_ROLE_PERMISSION}
+                                value="assign_help"
+                                className="w-4 h-4 bg-(--color-si-input-bg) accent-(--color-si-btn) cursor-pointer"
+                            />
+                            Przypisywanie pomocników
+                        </div>
+
+                        <div className="flex gap-1 items-center flex-1/2">
+                            <input
+                                type="checkbox"
+                                ref={rolePermChangeStatusRef}
+                                name={FORM_ROLE_PERMISSION}
+                                value="change_status"
+                                className="w-4 h-4 bg-(--color-si-input-bg) accent-(--color-si-btn) cursor-pointer"
+                            />
+                            Zmiana statusu
+                        </div>
+
+                        <div className="flex gap-1 items-center flex-1/2">
+                            <input
+                                type="checkbox"
+                                ref={rolePermMakeRoleRef}
+                                name={FORM_ROLE_PERMISSION}
+                                value="make_role"
+                                className="w-4 h-4 bg-(--color-si-input-bg) accent-(--color-si-btn) cursor-pointer"
+                            />
+                            Tworzenie ról
+                        </div>
+
+                        <div className="flex gap-1 items-center flex-1/2">
+                            <input
+                                type="checkbox"
+                                ref={rolePermChangeRoleRef}
+                                name={FORM_ROLE_PERMISSION}
+                                value="change_role"
+                                className="w-4 h-4 bg-(--color-si-input-bg) accent-(--color-si-btn) cursor-pointer"
+                            />
+                            Edytowanie ról
+                        </div>
+
+                        <div className="flex gap-1 items-center flex-1/2">
+                            <input
+                                type="checkbox"
+                                ref={rolePermAssignToProjectRef}
+                                name={FORM_ROLE_PERMISSION}
+                                value="assign_to_project"
+                                className="w-4 h-4 bg-(--color-si-input-bg) accent-(--color-si-btn) cursor-pointer"
+                            />
+                            Przypisywanie do projektów
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex items-center justify-between">
+                    <button
+                        disabled={busy}
+                        onClick={() => {
+                            roleNameRef.current = null;
+                            rolePermWriteTicketRef.current = null;
+                            rolePermHelpRef.current = null;
+                            rolePermAssignHelpRef.current = null;
+                            rolePermChangeStatusRef.current = null;
+                            rolePermMakeRoleRef.current = null;
+                            rolePermChangeRoleRef.current = null;
+                            rolePermAssignToProjectRef.current = null;
+                            onHide();
+                        }}
+                        className="px-6 py-2
+                                bg-(--color-si-btn-error)
+                                hover:bg-(--color-si-btn-error-hover) shadow-lg
+                                text-white text-sm font-semibold rounded-lg cursor-pointer transition-colors duration-200">
+                        Anuluj
+                    </button>
+
+                    <input
+                        type="submit"
+                        value={busy ? "Tworzenie..." : "Utwórz"}
+                        disabled={busy}
+                        className="px-6 py-2
+                            bg-(--color-si-btn)
+                            hover:bg-(--color-si-btn-hover) shadow-lg
+                            disabled:opacity-60 text-white text-sm font-semibold rounded-lg cursor-pointer transition-colors duration-200"
+                    />
+                </div>
+
+                <input
+                    name={FORM_PROJECT_ID}
+                    type="hidden"
+                    value={project.id}/>
+                <input
+                    name={FORM_ACTION}
+                    type="hidden"
+                    value={FORM_ACTION_NEW_ROLE}/>
             </fetcher.Form>
         </Popup>
     );
--- Frontend/src/dashboard/forms.ts
+++ Frontend/src/dashboard/forms.ts
@@ -12,11 +12,15 @@ export const FORM_INCIDENT_PRIORITY = "incident_priority";
 export const FORM_INCIDENT_ASSIGNEES = "incident_priority";
 export const FORM_PROJECT_ID = "project_id";
 
+export const FORM_ROLE_NAME = "role_name";
+export const FORM_ROLE_PERMISSION = "role_permission";
+
 export const FORM_ACTION_NEW_PROJECT = "new_project";
 export const FORM_ACTION_INVITE_USER = "invite_user";
 export const FORM_ACTION_DELETE_ORGANIZATION = "delete_organization";
 
 export const FORM_ACTION_NEW_INCIDENT = "new_incident";
+export const FORM_ACTION_NEW_ROLE = "new_role";
 
 export const FORM_ACTION_CREATE_ORGANIZATION = "create_organization";
 export const FORM_ACTION_JOIN_ORGANIZATION = "join_organization";
